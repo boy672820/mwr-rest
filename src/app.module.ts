d849ea3c9320cfea12fb66136ff4237496ac064e
boy672820 <boy672820@gmail.com>
2021-02-03 22:58:33 +0900

    Connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateModule } from './create/create.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    CreateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
